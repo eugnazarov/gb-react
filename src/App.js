ee8f3eb9072992c5ef946996dd197e46747a332d
eugnazaroff <eugene.nazaroff@icloud.com>
2022-02-03 14:13:11 +0400

    lesson-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import Message from "./components/Message/Message";
+import React, { useEffect, useState } from "react";
+
+import MessageList from "./components/MessageList/MessageList";
+import useInput from "./components/hooks/useInput";
+import "./App.css";
 
 const App = () => {
-    return (
-        <div>
-          <Message text="Привет!"/>
-        </div>
-    );
+  const input = useInput();
+
+  const [messageList, setMessageList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newMessage = {
+      author: "John",
+      text: input.value,
+    };
+    setMessageList((prevState) => [...prevState, newMessage]);
+    input.setValue("");
+  };
+
+  useEffect(() => {
+    if (
+      messageList[messageList.length - 1] &&
+      messageList[messageList.length - 1].author !== "robot"
+    ) {
+      const newMessage = {
+        author: "robot",
+        text: "Hello from robot",
+      };
+      setTimeout(() => {
+        setMessageList((prevState) => [...prevState, newMessage]);
+      }, 1500);
+    }
+  }, [messageList]);
+  return (
+    <div className="App">
+      <MessageList messageList={messageList} />
+      <form style={{ margin: "10px" }} onSubmit={handleSubmit}>
+        <p>Введите текст сообщения</p>
+        <input
+          style={{ padding: "10px", margin: "10px" }}
+          value={input.value}
+          onChange={({ target }) => input.setValue(target.value)}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/hooks/useInput.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+
+const useInput = () => {
+    const [value, setValue] = useState("")
+    return {value, setValue}
+};
+
+export default useInput;
