d331b9cf45c56ec0a6c290bb7fef929133415f10
eugnazaroff <eugene.nazaroff@icloud.com>
2022-02-14 20:07:58 +0500

    lesson-5

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Checkbox } from "@mui/material";
+import { TOGGLE } from "../store/profile/types";
 
 const Profile = () => {
+  const { isChecked } = useSelector((state) => state.profile);
+  const dispatch = useDispatch();
   return (
     <div>
       <p>Profile Page</p>
+      <Checkbox
+        value={isChecked}
+        onChange={() => {
+          dispatch({ type: TOGGLE });
+        }}
+      />
+      <p>{isChecked.toString()}</p>
     </div>
   );
 };
