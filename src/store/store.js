c2e2a448e5973100d2f2ef44b6c5fdb51ad8d70c
eugnazaroff <eugene.nazaroff@icloud.com>
2022-02-24 18:48:40 +0400

    lesson-8

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,13 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import { ProfileReducer } from "./profile/profileReducer";
 import { chatReducer } from "./chats/chatReducer";
+import { postsReducer } from "./posts/postsReducer";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   profile: ProfileReducer,
   chats: chatReducer,
+  posts: postsReducer,
 });
 
-export const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
